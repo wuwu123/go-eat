dd422aa700c4214d10622d0a67a3419ad7700b61
wj <1224805548@qq.com>
2018-08-23 14:49:05 +0800

    初始化框架

--- common/redis.go
+++ common/redis.go
@@ -2,13 +2,27 @@ package common
 
 import (
 	"github.com/go-redis/redis"
+	"github.com/astaxie/beego"
+	"fmt"
 )
 
-func GetRedis() *redis.Client  {
+func GetRedis() *redis.Client {
+	host := beego.AppConfig.String("cache_host")
+	port := beego.AppConfig.DefaultInt("cache_port", 6379)
+	password := beego.AppConfig.DefaultString("cache_pwd", "")
+	database := beego.AppConfig.DefaultInt("cache_database", 0)
+	if host == "" {
+		beego.Error("redis host 无效->", host)
+	}
+	addr := fmt.Sprintf("%s:%d" , host , port)
 	client := redis.NewClient(&redis.Options{
-		Addr:     "47.100.82.54:6379",
-		Password: "ky1024",
-		DB:       1,
+		Addr:     addr,
+		Password: password,
+		DB:       database,
 	})
+	_, err := client.Ping().Result()
+	if err != nil {
+		beego.Error("redis 建立链接失败")
+	}
 	return client
 }
